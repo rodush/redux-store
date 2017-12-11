4ed7df461c1a96e02c92dd3cecd2f5b57f7df64a
Roman Dushko <rodush@gmail.com>
2017-12-11 21:04:35 +0100

    Complete 'custom redux' based very primitive app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import * as fromStore from './store';
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   'click',
@@ -12,16 +19,28 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch(new fromStore.AddTodo(payload))
 
     input.value = '';
   },
   false
 );
 
+const unsubsribe = store.subscribe((state) => {
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener('click', unsubsribe, false);
+
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+    try {
+      const todo = JSON.parse(target.getAttribute('data-todo') as any);
+      store.dispatch(new fromStore.RemoveTodo(todo));
+    }
+    catch (e) {
+      return;
+    }
   }
 });
